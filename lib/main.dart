e618acf8b85c6c61d85c384d268b9e9d8f6d4d39
suishanwen <suishanwen@icloud.com>
2019-01-09 16:20:43 +0800

    add redux_logging middleware

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_thunk/redux_thunk.dart';
+import 'package:redux_logging/redux_logging.dart';
 import 'reducer/combineRecuder.dart';
 import 'model/user.dart';
 import 'model/page.dart';
@@ -9,7 +10,7 @@ import 'reduxApp.dart';
 void main() {
   final store = new Store<AppState>(
     appReducer,
-    middleware: [thunkMiddleware],
+    middleware: [thunkMiddleware,new LoggingMiddleware.printer()],
     initialState: new AppState(
       user: new User(false, '', null),
       page: new Page(false, 0, null),
