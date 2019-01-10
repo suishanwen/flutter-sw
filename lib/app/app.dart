53c05c27ffeb96adf129b07a70b9df953f7d2440
suishanwen <suishanwen@icloud.com>
2019-01-10 14:28:40 +0800

    rm debug banner

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -7,6 +7,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Navigation',
+      debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: <String, WidgetBuilder>{
         '/': (BuildContext context) => new Login(),
